5d26136fffabf22915906d3173162e96b3bf1955
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-07-11 12:27:43 -0700

    Fix DeclareDatatypes with SMT-LIB 2.6
    Apparently, 2.6 is the first version to officially introduce datatypes,
    and in the process they changed the syntax compared to 2.5.

--- test/test.js
+++ test/test.js
@@ -24,7 +24,23 @@ function test2() {
         console.error('FAILED: ' + e.message);
     });
 }
+function test3() {
+    let solver = new smt.LocalCVC4Solver('QF_ALL_SUPPORTED');
+    solver.enableAssignments();
+    solver.add(smt.DeclareDatatype('MyType', ['C1', 'C2']));
+    solver.add(smt.DeclareFun('x', [], 'Bool'))
+    solver.add(smt.DeclareFun('y', [], 'Bool'));
+    solver.add(smt.DeclareFun('z', [], 'MyType'));
+    solver.assert(smt.And(smt.Or('x', 'y'), 'x', 'y'));
+    return solver.checkSat().then(([sat, assignment]) => {
+        assert(sat);
+        assert(assignment.x);
+        assert(assignment.y);
+    }).catch((e) => {
+        console.error('FAILED: ' + e.message);
+    });
+}
 function main() {
-    test1().then(() => test2());
+    test1().then(() => test2()).then(() => test3());
 }
 main();
